74a52cb15faa9d69c56c14f23988fc0cb76ad9da
KimKyeongSeog <kimks9241@naver.com>
2023-11-13 16:54:24 +0900

    second commit

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+import CreateTodo from "../components/CreateTodo";
+
 const Main = () => {
-    return <div className="bg-red-100">Main</div>;
-  };
+
+  const [todos, setTodos] = useState([]);
+  const [lastTodoID, setLastTodoId] = useState(0);
   
-  export default Main;
+  const getTodos = () => {
+    const localTodos = localStorage.getItem("todos")
+
+    if (!localTodos) return;
+
+    const parsedTodos = JSON.parse(localTodos)
+
+    setTodos(parsedTodos);
+
+    setLastTodoId(parsedTodos[parsedTodos.length - 1].id);
+ 
+  };
+
+// 로컬스토리지로부터 읽어오는 함수 필요 비어있는 대괄호는 전개구문 시 배열이 비어있는 상태도 포함시키기 위함
+// localTodos의 값이 없으면 돌아오게 if~ return 설정
+
+useEffect(() => {
+  getTodos();  
+}, []);
+
+  return (
+  
+  <main className="bg-gray-400 min-h-screen-md mx-auto">
+    <h1 className="bg-blue-100 text-center text-4xl font-bold py-12">
+    To do List
+    </h1>
+    <CreateTodo todos={todos} getTodos={getTodos} lastTodoID={lastTodoID}/>
+    <ul className="bg-cyan-100 w-96 mx-auto mt-12 h-[480px] overflow-y-auto">
+      <li className="h-12 flex items-center text-xl">
+        <span className="w-1/12 text-right">1</span>
+        <span className="w-8/12 pl-2">💻 코딩하기</span>
+        <button className="w-3/12 hover:font-bold">Detail</button>
+      </li>
+    </ul>
+  </main>
+  );
+};
+
+export default Main;
