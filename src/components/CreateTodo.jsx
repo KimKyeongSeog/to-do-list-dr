aa9ed1f11560083014a19c5a14f6f66db88e96b3
KimKyeongSeog <kimks9241@naver.com>
2023-11-13 19:04:39 +0900

    1113end

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const CreateTodo = ({ todos, getTodos, lastTodoID }) => {
+  const [newTodo, setNewTodo] = useState("");
+
+  const onSubmitNewTodo = (e) => {
+    e.preventDefault();
+// 제출한 값을 추가
+    if (!newTodo) return;
+//값 없을 떄 return
+    const newTodos = [
+      ...todos,
+      { id: lastTodoID + 1, title: newTodo, isDone: false },
+    ];
+//새로운 배열이 추가될 때 , 기존 todo값에서 새로운 todo를 추가함.
+    localStorage.setItem("todos", JSON.stringify(newTodos)); 
+    //새로운 배열을 저장하기 위함, 로컬스토리지는 문자열로 저장되기 떄문에 전환도 같이함
+
+    getTodos(); //저장한 값을 가져오기 위함
+    setNewTodo(""); //내용을 등록 후 초기값으로 재설정하기 위함
+  };
+
+  return (
+    <form
+      className="w-96 mx-auto mt-12 flex"
+      onSubmit={onSubmitNewTodo}
+    >
+      <input
+        className="w-3/4 mr-4 rounded-md p-2 focus:outline-none border-2 focus:border-blue-300"
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <input
+        className="w-1/4 bg-blue-400 hover:bg-blue-600 active:bg-blue-400 rounded-md text-white font-semibold"
+        type="submit"
+        value="생 성"
+      />
+    </form>
+  );
+};
+
+export default CreateTodo;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,5 +1,54 @@
+import { useEffect, useState } from "react";
+import CreateTodo from "../components/CreateTodo";
+
 const Main = () => {
-    return <div className="bg-red-100">Main</div>;
-  };
+
+  const [todos, setTodos] = useState([]);
+  const [lastTodoID, setLastTodoId] = useState(0);
   
-  export default Main;
+  const getTodos = () => {
+    const localTodos = localStorage.getItem("todos")
+
+    if (!localTodos) return;
+
+    const parsedTodos = JSON.parse(localTodos)
+
+    setTodos(parsedTodos);
+
+    setLastTodoId(parsedTodos[parsedTodos.length - 1].id);
+ 
+  };
+
+// 로컬스토리지로부터 읽어오는 함수 필요 비어있는 대괄호는 전개구문 시 배열이 비어있는 상태도 포함시키기 위함
+// localTodos의 값이 없으면 돌아오게 if~ return 설정
+
+useEffect(() => {
+  getTodos();  
+}, []);
+
+  return (
+  
+  <main className="bg-gray-400 min-h-screen mx-auto">
+    <h1 className="bg-blue-100 text-center text-4xl font-bold py-12">
+    To do List
+    </h1>
+    <CreateTodo todos={todos} getTodos={getTodos} lastTodoID={lastTodoID}/>
+    <ul className="bg-green-200 w-96 mx-auto mt-12 h-[30rem] overflow-y-auto">
+        {todos.length === 0
+        // 배열의 경우. 문자열이나 숫자열 값을 입력할 때 처럼 !를 사용하면 안됨!
+          ? "비어있을 때"
+          : todos.map((v, i) => {
+              return (
+                <li key={i} className="h-12 flex items-center text-xl">
+                  <span className="w-1/12 text-right">{v.id}</span>
+                  <span className="w-8/12 pl-2">{v.title}</span>
+                  <button className="w-3/12 hover:font-bold">Detail</button>
+                </li>
+              );
+            })}
+      </ul>
+  </main>
+  );
+};
+
+export default Main;
